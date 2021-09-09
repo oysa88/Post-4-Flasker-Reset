a5f1ec606bebe9d13852b15d0eb398dc667ab734
Øystein Salomonsen <oystein.salomonsen@tekniskmuseum.no>
2021-09-09 20:35:47 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("Reset", 0)
+    basic.showLeds(`
+        . # . . .
+        # # # # .
+        . # . . #
+        . . . . #
+        . . # # .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
 })
+radio.setGroup(4)
